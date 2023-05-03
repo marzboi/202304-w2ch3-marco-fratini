f167ca37e1f6c45de79c29ec3bef2338bab53d84
Marco Fratini <marcofratini@outlook.com>
2023-05-03 21:14:39 +0200

    Add second testing condition to function

--- app/connect4.test.js
+++ app/connect4.test.js
@@ -17,11 +17,16 @@ describe("Given the function generateBoardLogic", () => {
   });
 
   describe("When given the values of 1 and 1", () => {
-    test("Then it should return a board with 1 row and 1 column", ()=> {
+    test("Then it should return a board with 1 row and 1 column", () => {
       const rows = 1;
       const columns = 1;
 
-      const expectedTable = 
-    })
-  })
+      const expectedTable = [
+        [{ matched: false, player: "", rowLocation: 1, colLocation: 1 }],
+      ];
+      const returnedTable = connect4.generateBoardLogic(rows, columns);
+
+      expect(returnedTable).toStrictEqual(expectedTable);
+    });
+  });
 });
